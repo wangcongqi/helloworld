67358aaf9e755d7c2cda721103485f09155a70d2
wangcongqi <1239942433@qq.com>
2022-11-12 15:01:25 +0800

    first commit

---
+++ src/main/java/com/atguigu/helloworld/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.atguigu.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("")
+    public String test() {
+        System.out.println("Hello World");
+        return ("OK");
+    }
+
+}
